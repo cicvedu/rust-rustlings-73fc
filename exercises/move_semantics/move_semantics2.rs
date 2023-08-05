15e9f024d332e89b0434f3cdf5c6601813ddfffc
73fc <542591851@qq.com>
2023-08-06 00:53:01 +0800

    3 c

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -11,7 +11,7 @@
 fn main() {
     let vec0 = Vec::new();
 
-    let  mut vec1 = fill_vec(& vec0);
+    let  mut vec1 = fill_vec( &vec0);
 
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
@@ -20,7 +20,7 @@ fn main() {
     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: & Vec<i32>) ->   Vec<i32> {
+fn fill_vec(vec:  &Vec<i32>) ->   Vec<i32> {
     let mut vec = vec.clone();
 
     vec.push(22);
--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -6,10 +6,11 @@
 // Execute `rustlings hint move_semantics3` or use the `hint` watch subcommand
 // for a hint.
 
-fn main() {
-    let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+fn main() {
+    let  vec0 = Vec::new();
+
+    let mut vec1 = fill_vec( vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -18,11 +19,10 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec : Vec<i32>) -> Vec<i32> {
-    let mut vec = vec.clone();
+fn fill_vec(mut vec:  Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec.clone()
+    vec
 }
